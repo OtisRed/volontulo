abed5dfa3d407d3ba1108c69ad80ce2ae27288c3
OtisRed <durczak.krzysztof@gmail.com>
2018-08-01 22:02:09 +0200

    Working with Radek

--- frontend/src/app/offers/offer-join-form/offer-join-form.component.ts
+++ frontend/src/app/offers/offer-join-form/offer-join-form.component.ts
@@ -1,10 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {filter, take} from 'rxjs/operators';
+import {catchError} from 'rxjs/operators';
 import {User} from '../../user';
 import {ActivatedRoute} from '@angular/router';
 import {UserService} from '../../user.service';
 import {AuthService} from '../../auth.service';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import { map, mergeMap } from 'rxjs/operators';
+import {of} from "rxjs/observable/of";
 
 @Component({
   selector: 'volontulo-offer-join-form',
@@ -15,25 +19,24 @@ export class OfferJoinFormComponent implements OnInit {
   public joinForm: FormGroup = this.fb.group( {
     applicant_email: ['', [Validators.required, Validators.email]],
     applicant_name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(150)]],
-    message: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(2000)]],
+    message: ['', [Validators.minLength(10), Validators.maxLength(2000)]],
     phone_no: ['', [Validators.maxLength(20)]],
     });
 
   public submitEnabled = false;
+  public success: null | boolean = null;
 
   constructor(
     private route: ActivatedRoute,
     private fb: FormBuilder,
     private authService: AuthService,
     private userService: UserService,
+    private httpClient: HttpClient,
   ) {}
 
   ngOnInit() {
     this.authService.user$
-      .pipe(
-        filter(user => user !== null),
-        take(1),
-      )
+
       .subscribe(
         (user: User) => {
           this.joinForm.controls.applicant_email.setValue(user.email);
@@ -43,4 +46,16 @@ export class OfferJoinFormComponent implements OnInit {
       )
   }
 
+  onSubmit() {
+    if (this.joinForm.valid) {
+      console.log(this.joinForm.value)
+
+      this.route.params.pipe(
+        map(params => params.offerId),
+        mergeMap(offerId => this.httpClient.post(`${environment.apiRoot}/offers/${offerId}/join`, this.joinForm.value)),
+        catchError(err => {alert(err); return of(err)}))
+        .subscribe(
+        () => {this.success = true})
+        }
+  }
 }
